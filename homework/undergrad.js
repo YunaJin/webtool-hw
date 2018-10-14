971e7ba2c20b0e1fbb0553876f021efe79bf820d
yuanyuan Jin <31752309+YunaJin@users.noreply.github.com>
2018-10-15 03:35:20 +0800

    Add files via upload

---
+++ homework/undergrad.js
@@ -0,0 +1,18 @@
+/*
+ * Using NodeJS syntax, export an ES6 class-syntax class that extends Student
+ * Your class should call super() to set the name
+ * Your class should define a degree function that returns 'BS'
+ * Export the class you create
+ *
+*/
+
+const Student = require('./student');
+
+class Undergrad extends Student {
+    degree() {
+        return "BS"
+    }
+
+}
+
+module.exports = Undergrad;
